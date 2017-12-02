1e41361c4e1d0dd0497854e23c1d90892c8e8c62
giullianogp <giullianogp@giullianogps-iMac.local>
2017-12-02 16:06:44 -0200

    adiciona dados

---
+++ MeuPet/MeuPet/PetDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  PetDataManager.swift
+//  MeuPet
+//
+//  Created by giullianogp on 02/12/17.
+//  Copyright © 2017 senac. All rights reserved.
+//
+
+import UIKit
+
+class PetDataManager: NSObject {
+
+}
