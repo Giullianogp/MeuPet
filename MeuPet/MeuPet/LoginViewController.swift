54dc63ca3a2ad6f8d4974c0a8fadc66b2e9a3721
eduardo thome <eduardocanninithome@hotmail.com>
2017-12-02 11:30:26 -0200

    alteracoes tela login

--- MeuPet/MeuPet/LoginViewController.swift
+++ MeuPet/MeuPet/LoginViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var txtLogin: UITextField!
     
+    @IBOutlet weak var messageInfo: UILabel!
     @IBOutlet weak var imgLogin: UIImageView!
     @IBOutlet weak var txtPassword: UITextField!
     
@@ -26,8 +28,32 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnLogin(_ sender: Any) {
+        
+        if (txtLogin.text?.isEmpty)! || (txtPassword.text?.isEmpty)! {
+            messageInfo.text = "Preencha o email/senha"
+            return
+        } else {
+            messageInfo.text = " "
+        }
+        
+        
+        //let parameters: [String: Any] = [
+        //"login" : "",
+        //"senha" : "iosclient"
+        //]
+        
+        //Alamofire.request("https://httpbin.org/get", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            
+        //}
+        
+        performSegue(withIdentifier: "segueLogin", sender: sender)
+    
+    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //envia os dados recebidos da autenticação da API
      
     }
 
