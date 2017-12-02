5dd8527f58b624f25182baa6ebc0747c1a7a2331
Rodrigo Bremm <Rodrigo Bremm>
2017-12-02 14:43:52 -0200

    mainstoryboard-dividido-3

--- MeuPet/MeuPet/AppDelegate.swift
+++ MeuPet/MeuPet/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let qual = 2
+        let qual = 1
         
         var vc: UIViewController
         if qual == 1 {
